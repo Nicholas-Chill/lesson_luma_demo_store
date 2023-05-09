b1fb3f417934f0486f72b3ddf3b0fa2124bea45e
Nicholas Chill <11b3827@gmail.com>
2023-05-09 12:38:11 +0200

    new commit

---
+++ test/searchForProducts.js
@@ -0,0 +1,47 @@
+// includes
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const should = require('chai').should();
+
+/* 
+As a customer, 
+I want to be able to search for a product,
+so that I can find the product I want to buy
+*/
+
+describe('Search for a product', () => {
+    context('I search for a product', () => {
+        it('I should see the product that I have searched for', async () => {
+            // Satart web driver
+            const driver = await new Builder().forBrowser('firefox').build();
+            
+            // Search for a product
+            try {
+                // Move to magento site
+                await driver.get('https://magento.softwaretestingboard.com/');
+
+                // Get the search input
+                await driver.wait(until.elementLocated(By.css('#search')), 10000);
+                await driver.findElement(By.id('search')).sendKeys('shirt', Key.RETURN);
+
+                // Find the first product
+                await driver.wait(until.elementLocated(By.css('.product-item:first-child')), 10000);
+                const product = await driver.findElement(By.css('.product-item:first-child'));
+                
+                //Find information in the product we just selected
+                let productTitle = await product.findElement(By.css('.product-item-link'));
+                let productPrice = await product.findElement(By.css('.price'));
+
+                // Extract text
+                let productTitleText = await productTitle.getText();
+                let productPriceText = await productPrice.getText();
+
+                productTitleText.should.equal('Radiant Tee');
+                productPriceText.should.equal('$22.00');
+            } catch(error) {
+                console.log(error);
+            } finally {
+                await driver.quit();
+            }
+        });
+    });
+});
---
+++ test/testLogin.js
@@ -0,0 +1,50 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const {should} = require('chai').should();
+require('dotenv').config();
+
+// Login data
+let user = process.env.USER;
+let pass = process.env.PASS;
+
+/* 
+    As a user of Luma Webstore,
+    I would like to be able to login,
+    so that I can see my profile. 
+*/
+
+// .only() runs only this test block
+// .skip() skips this specific test
+describe.only('Login to Luma Demostore', () => {
+    context('I click on login and enter my credentials', () => {
+        it('I should be logged in and see my profile', async () => {
+            const driver = await new Builder().forBrowser('firefox').build();
+            try {
+                await driver.get('https://magento.softwaretestingboard.com');
+                await driver.findElement(By.css('.authorization-link > a:nth-child(1)')).click();
+
+                await driver.wait(until.elementLocated(By.id('email')), 10000);
+                await driver.findElement(By.id('email')).sendKeys(user);
+                await driver.findElement(By.id('pass')).sendKeys(pass);
+                await driver.findElement(By.css('#send2')).click();
+
+                await driver.sleep(1000);
+                
+                await driver.wait(until.elementLocated(By.css('.action.switch')), 20000);
+                await driver.findElement(By.css('.action.switch')).click();
+
+                await driver.wait(until.elementLocated(By.css('a[href$="/customer/account/"]')), 10000);
+                await driver.findElement(By.css('a[href$="/customer/account/"]')).click();
+
+                await driver.wait(until.elementLocated(By.css('.box-information .box-content p')), 10000);
+                const information = await driver.findElement(By.css('.box-information .box-content p')).getText();
+
+                // Assert
+                information.should.contain('Test Testsson');
+            } catch(error) {
+                console.log(error);
+            } finally {
+                await driver.quit();
+            }
+        });
+    });
+});
